d1bcd089ca8d5218a45dfd4e918c842ce93d1f88
123ABm <94301088+123ABm@users.noreply.github.com>
2025-11-20 00:14:46 +0800

    feat:底部tab实现

---
+++ computer-mini/src/pages/discover/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <view class="page">
+    <text class="title">发现</text>
+  </view>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.page {
+  padding: 20rpx;
+}
+.title {
+  font-size: 32rpx;
+}
+</style>
